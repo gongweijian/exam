39238622a3f553c789091f4ecb8ba199b3590d5d
gongweijian <564651010@qq.com>
2018-08-08 18:58:07 +0800

    add

---
+++ src/github.com/gongweijian/exam/word_test.go
@@ -0,0 +1,28 @@
+package word
+// Package word provides utilities for word games.
+import "testing"
+// IsPalindrome reports whether s reads the same forward and backward.
+// (Our first attempt.)
+
+func IsPalindrome(s string) bool {
+for i := range s {
+if s[i] != s[len(s)-1-i] {
+return false
+}
+}
+return true
+}
+
+func TestPalindrome(t *testing.T) {
+if !IsPalindrome("detartrated") {
+t.Error(`IsPalindrome("detartrated") = false`)
+}
+if !IsPalindrome("kayak") {
+t.Error(`IsPalindrome("kayak") = false`)
+}
+}
+func TestNonPalindrome(t *testing.T) {
+if IsPalindrome("palindrome") {
+t.Error(`IsPalindrome("palindrome") = true`)
+}
+}
